93c696a7b7ad734d9d3ae29cbf4c0c650aec9809
fanzehua <316679581@qq.com>
2019-06-11 11:45:09 +0800

    修改showModel样式

--- src/components/showModel.vue
+++ src/components/showModel.vue
@@ -1,11 +1,11 @@
 <template>
   <el-dialog :title="title || '提示'" :visible="dialogVisible" @close="closeModel">
-    <div class="wrap">
+    <div>
       <slot name="content"></slot>
     </div>
-    <div slot="footer">
-      <el-button @click="doCancel">{{cancel || '取消'}}</el-button>
-      <el-button type="primary" @click="doConfirm">{{confirm||'确定'}}</el-button>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="doCancel" class="btn">{{cancel || '取消'}}</el-button>
+      <el-button class="btn" type="primary" @click="doConfirm">{{confirm||'确定'}}</el-button>
     </div>
   </el-dialog>
 </template>
@@ -26,4 +26,14 @@ export default {
 };
 </script>
 <style lang='scss' scoped>
+.dialog-footer {
+  display: flex;
+  width: 250px;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto 20px auto;
+}
+.btn {
+  width: 100px;
+}
 </style>
