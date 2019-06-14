97e5bd9f6ce9b1e25014e93c4d478f792b56486d
fanzehua <316679581@qq.com>
2019-06-14 11:25:47 +0800

    修改默认ip地址

--- config/index.js
+++ config/index.js
@@ -17,7 +17,7 @@ module.exports = {
         },
 
         // Various Dev Server settings
-        host: '192.168.0.115', // can be overwritten by process.env.HOST
+        host: '192.168.0.118', // can be overwritten by process.env.HOST
         port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
         autoOpenBrowser: false,
         errorOverlay: true,
