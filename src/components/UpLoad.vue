66c236ce2cb5d315d71fc9092938eeecaa7f000c
fanzehua <316679581@qq.com>
2019-06-14 11:28:30 +0800

    添加图片上传组件

---
+++ src/components/UpLoad.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="contain">
+    <el-upload
+      class="avatar-uploader"
+      :show-file-list="false"
+      ref="upload"
+      action="/api/upload"
+      :http-request="upload"
+      multiple
+    >
+      <img v-if="imageUrl" :src="imageUrl" class="avatar">
+      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+    </el-upload>
+  </div>
+</template>
+<script>
+export default {
+    name: 'upload',
+    data () {
+        return {
+            imageUrl: ''
+        };
+    },
+    methods: {
+        upload () {
+            const formData = new FormData();
+            const uploadFiles = this.$refs.upload.uploadFiles;
+            const fileIndex = uploadFiles.length - 1;
+            const file = uploadFiles[fileIndex];
+            const headerConfig = {
+                headers: { 'Content-Type': 'multipart/form-data' }
+            };
+            formData.append('image', file.raw);
+            this.$http.post('/api/upload', formData, headerConfig).then(res => {
+                let { success, url } = res;
+                this.imageUrl = this.$API + url;
+            });
+        }
+    }
+};
+</script>
+<style lang="scss" scoped>
+.contain {
+  display: flex;
+  flex-direction: column;
+}
+.avatar-uploader {
+  display: flex;
+  width: 200px;
+  height: 200px;
+  align-items: center;
+  justify-content: center;
+  border: 1px dashed #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 200px;
+  height: 200px;
+}
+</style>
