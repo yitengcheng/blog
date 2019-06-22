03d9ee3c809138a156a46ffd5e6e93afb8657125
fanzehua <316679581@qq.com>
2019-06-22 16:29:01 +0800

    增加菜单栏权限控制

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -13,6 +13,7 @@
       v-for="(item,index) in menuList"
       :key="index"
       :index="item.path"
+      v-show="(item.showFlag)"
     >{{item.title}}</el-menu-item>
     <div class="imageContainer">
       <el-image src="static/rbq.png" fit="contain" class="foot-image"></el-image>
@@ -22,18 +23,67 @@
 
 <script>
 import Login from './Login';
+import { mapState } from 'vuex';
 export default {
     components: { Login },
     data () {
         return {
             activeIndex: '/',
             menuList: [
-                { path: '/', title: '简历' },
-                { path: '/cssPhoto', title: 'CSS动画' },
-                { path: '/calendar', title: '日历' },
-                { path: '/knowledge', title: '文字标注' }
+                {
+                    path: '/',
+                    title: '简历',
+                    showFlag: true,
+                    hasJudge: false
+                },
+                {
+                    path: '/cssPhoto',
+                    title: 'CSS动画',
+                    showFlag: true,
+                    hasJudge: false
+                },
+                {
+                    path: '/calendar',
+                    title: '日历',
+                    showFlag: true,
+                    hasJudge: false
+                },
+                {
+                    path: '/knowledge',
+                    title: '文字标注',
+                    showFlag: true,
+                    hasJudge: false
+                },
+                {
+                    path: '/imageList',
+                    title: '图片浏览',
+                    showFlag: false,
+                    hasJudge: true
+                }
             ]
         };
+    },
+    computed: {
+        ...mapState({
+            userInfo: state => state.user.user
+        })
+    },
+    methods: {
+        refeshList (value) {
+            this.menuList.forEach(item => {
+                if (item.hasJudge) {
+                    let flag = this._.findIndex(value.permissions, o => {
+                        return o === 'watchImage';
+                    });
+                    item.showFlag = flag !== -1;
+                }
+            });
+        }
+    },
+    watch: {
+        userInfo (val, oldVal) {
+            this.refeshList(val);
+        }
     }
 };
 </script>
