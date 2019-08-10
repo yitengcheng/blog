6d5abc0bdf439b9906d69d82afecef8a6393b310
fanzehua <316679581@qq.com>
2019-08-10 10:36:10 +0800

    修改简历页面，拆分侧边栏菜单

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,7 @@ import StoreLoading from './../views/CSSCartToon/StoreLoading';
 Vue.use(Router);
 
 export default new Router({
+    mode: 'history',
     routes: [
         {
             path: '/',
--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -21,6 +21,8 @@
         <div class="experienceText">从2017-2019在贵州亿隆四面通担任网页与手机APP的研发工作</div>
         <div class="experienceTitle">平台运维</div>
         <div class="experienceText">从2019至今在贵州亿隆智达网络有限公司担任平台运维</div>
+        <div class="experienceTitle">系统开发</div>
+        <div class="experienceText">从2019至今在贵州亿隆智达网络有限公司担任办公软件开发负责人</div>
         <div class="experience">S K I L L S</div>
         <div class="skillWarrper">
           <div class="skill">HTML5</div>
@@ -33,6 +35,57 @@
           <div class="skill">小程序</div>
         </div>
       </div>
+    </div>
+    <div class="pastProjectsContain">
+      <h2 class="pastProjects">过往项目</h2>
+      <p class="pastProjectsTitle">四面通平台手机APP</p>
+      <p class="pastProjectsText">工作职责：</p>
+      <p class="pastProjectsText">
+        <span style="marginLeft:40px"></span>
+        负责司机端，客户端，店面端页面搭建与业务逻辑编写，集成其他第三方库，带领小组攻克技术难点。
+        负责审核小组成员代码。负责项目中所有公用代码封装及优化，配合后端完成项目中测试环境与线上环境的数据完整性
+      </p>
+      <p class="pastProjectsText">工作业绩：</p>
+      <p class="pastProjectsText">
+        <span style="marginLeft:40px"></span>使用reactNative及周边技术栈对项目进行编写，提高项目可维护性
+      </p>
+      <p class="pastProjectsTitle">四面通平台网页端</p>
+      <p class="pastProjectsText">工作职责：</p>
+      <p class="pastProjectsText">
+        <span style="marginLeft:40px"></span>负责客户端，店面端页面搭建与业务逻辑编写
+      </p>
+      <p class="pastProjectsText">工作业绩：</p>
+      <p class="pastProjectsText">
+        <span style="marginLeft:40px"></span>使用react.js及周边技术栈对项目进行编写，学会了使用react的相关知识与语法
+      </p>
+      <p class="pastProjectsTitle">四面通平台小程序</p>
+      <p class="pastProjectsText">工作职责：</p>
+      <p class="pastProjectsText">
+        <span style="marginLeft:40px"></span>负责业务员小程序页面搭建与业务逻辑编写
+      </p>
+      <p class="pastProjectsText">工作业绩：</p>
+      <p class="pastProjectsText">
+        <span style="marginLeft:40px"></span>使用微信小程序原生语言对项目进行编写，学会了微信小程序的相关内容
+      </p>
+      <p class="pastProjectsText">工作业绩：</p>
+      <p class="pastProjectsText">
+        <span style="marginLeft:40px"></span>使用react.js及周边技术栈对项目进行编写，学会了使用react的相关知识与语法
+      </p>
+      <p class="pastProjectsTitle">亿隆集团OA办公系统</p>
+      <p class="pastProjectsText">工作职责：</p>
+      <p class="pastProjectsText">
+        <span style="marginLeft:40px"></span>
+        负责项目前端页面搭建与业务逻辑编写。负责项目技术选型，搭建项目框架，集成第三方库。
+        审核小组成员代码。负责项目中所有公用代码封装及优化。负责服务端的逻辑编写，数据库搭建。负责项目部署与实施。负责与其他部门沟通，分析不同部门业务需求
+      </p>
+      <p class="pastProjectsText">工作业绩：</p>
+      <p class="pastProjectsText">
+        <span style="marginLeft:40px"></span>
+        使用Vue及周边技术栈完成项目，前后端分离提高前后端开发效率。
+        借助Vuex存储数据的状态，划分model数据使项目数据流更清晰。
+        接入第三方excel生成工具实现项目表格导出。
+        封装可复用组件及功能函数，方便重复使用
+      </p>
     </div>
   </div>
 </template>
@@ -126,5 +179,26 @@ export default {
   border: 1px solid #333;
   border-radius: 50%;
   margin-right: 20px;
+}
+.pastProjectsContain {
+  display: flex;
+  flex-direction: column;
+  width: 800px;
+  margin-left: 180px;
+  margin-bottom: 50px;
+  align-self: center;
+}
+.pastProjects {
+  font-size: 18px;
+  line-height: 40px;
+  align-self: center;
+}
+.pastProjectsTitle {
+  font-size: 16px;
+  line-height: 30px;
+}
+.pastProjectsText {
+  font-size: 14px;
+  line-height: 30px;
 }
 </style>
