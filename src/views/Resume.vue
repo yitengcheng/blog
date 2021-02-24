efc79b298f58f36d08f303f9343c06a4669d40eb
fanzehua <316679581@qq.com>
2021-02-24 07:03:56 +0000

    修改个人简历

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -18,11 +18,21 @@
         <div class="experienceTitle">自主创业</div>
         <div class="experienceText">从2014-2015自主创业经营一家桌游卡牌店</div>
         <div class="experienceTitle">开发人员</div>
-        <div class="experienceText">从2017-2019在贵州亿隆四面通担任网页与手机APP的研发工作</div>
+        <div class="experienceText">
+          从2017-2019在贵州亿隆四面通担任网页与手机APP的研发工作
+        </div>
         <div class="experienceTitle">平台运维</div>
-        <div class="experienceText">从2019至今在贵州亿隆智达网络有限公司担任平台运维</div>
+        <div class="experienceText">
+          从2019至今在贵州亿隆智达网络有限公司担任平台运维
+        </div>
         <div class="experienceTitle">系统开发</div>
-        <div class="experienceText">从2019至今在贵州亿隆智达网络有限公司担任办公软件开发负责人</div>
+        <div class="experienceText">
+          从2019至今在贵州亿隆智达网络有限公司担任办公软件开发负责人
+        </div>
+        <div class="experienceTitle">项目经理</div>
+        <div class="experienceText">
+          从2020至今在贵州申瓯通信电子科技有限公司担任高级软件工程师，项目经理
+        </div>
         <div class="experience">S K I L L S</div>
         <div class="skillWarrper">
           <div class="skill">HTML5</div>
@@ -40,69 +50,94 @@
       <h2 class="pastProjects">过往项目</h2>
       <p class="pastProjectsTitle">四面通平台手机APP</p>
       <p class="pastProjectsText">工作职责：</p>
-      <p class="pastProjectsText">
-        <span style="marginLeft:40px"></span>
+      <p class="pastProjectsText" style="textIndent:2em">
         负责司机端，客户端，店面端页面搭建与业务逻辑编写，集成其他第三方库，带领小组攻克技术难点。
         负责审核小组成员代码。负责项目中所有公用代码封装及优化，配合后端完成项目中测试环境与线上环境的数据完整性。
       </p>
       <p class="pastProjectsText">工作业绩：</p>
-      <p class="pastProjectsText">
-        <span style="marginLeft:40px"></span>使用reactNative及周边技术栈对项目进行编写，提高项目可维护性。
+      <p class="pastProjectsText" style="textIndent:2em">
+          使用reactNative及周边技术栈对项目进行编写，提高项目可维护性。
       </p>
       <p class="pastProjectsTitle">四面通平台网页端</p>
       <p class="pastProjectsText">工作职责：</p>
-      <p class="pastProjectsText">
-        <span style="marginLeft:40px"></span>负责客户端，店面端页面搭建与业务逻辑编写。
+      <p class="pastProjectsText" style="textIndent:2em">
+          负责客户端，店面端页面搭建与业务逻辑编写。
       </p>
       <p class="pastProjectsText">工作业绩：</p>
-      <p class="pastProjectsText">
-        <span style="marginLeft:40px"></span>使用react.js及周边技术栈对项目进行编写，学会了使用react的相关知识与语法。
+      <p class="pastProjectsText" style="textIndent:2em">
+          使用react.js及周边技术栈对项目进行编写，学会了使用react的相关知识与语法。
       </p>
       <p class="pastProjectsTitle">四面通平台小程序</p>
       <p class="pastProjectsText">工作职责：</p>
-      <p class="pastProjectsText">
-        <span style="marginLeft:40px"></span>负责业务员小程序页面搭建与业务逻辑编写。
+      <p class="pastProjectsText" style="textIndent:2em">
+          负责业务员小程序页面搭建与业务逻辑编写。
       </p>
       <p class="pastProjectsText">工作业绩：</p>
-      <p class="pastProjectsText">
-        <span style="marginLeft:40px"></span>使用微信小程序原生语言对项目进行编写，学会了微信小程序的相关内容。
+      <p class="pastProjectsText" style="textIndent:2em">
+          使用微信小程序原生语言对项目进行编写，学会了微信小程序的相关内容。
       </p>
       <p class="pastProjectsText">工作业绩：</p>
-      <p class="pastProjectsText">
-        <span style="marginLeft:40px"></span>使用react.js及周边技术栈对项目进行编写，学会了使用react的相关知识与语法。
+      <p class="pastProjectsText" style="textIndent:2em">
+          使用react.js及周边技术栈对项目进行编写，学会了使用react的相关知识与语法。
       </p>
       <p class="pastProjectsTitle">亿隆集团OA办公系统</p>
       <p class="pastProjectsText">工作职责：</p>
-      <p class="pastProjectsText">
-        <span style="marginLeft:40px"></span>
+      <p class="pastProjectsText" style="textIndent:2em">
         负责项目前端页面搭建与业务逻辑编写。负责项目技术选型，搭建项目框架，集成第三方库。
         审核小组成员代码。负责项目中所有公用代码封装及优化。负责服务端的逻辑编写，数据库搭建。负责项目部署与实施。负责与其他部门沟通，分析不同部门业务需求。
       </p>
       <p class="pastProjectsText">工作业绩：</p>
-      <p class="pastProjectsText">
-        <span style="marginLeft:40px"></span>
+      <p class="pastProjectsText" style="textIndent:2em">
         使用Vue及周边技术栈完成项目，前后端分离提高前后端开发效率。
         借助Vuex存储数据的状态，划分model数据使项目数据流更清晰。
         接入第三方excel生成工具实现项目表格导出。
         封装可复用组件及功能函数，方便重复使用。
       </p>
+      <p class="pastProjectsTitle">惠水县明田社区大数据综合治理平台</p>
+      <p class="pastProjectsText">工作职责：</p>
+      <p class="pastProjectsText" style="textIndent:2em">
+        负责项目前端页面搭建与业务逻辑编写。负责与甲方沟通项目需求。负责项目相关文档编写。
+      </p>
+      <p class="pastProjectsText">工作业绩：</p>
+      <p class="pastProjectsText" style="textIndent:2em">
+        配合当地派出所为中央检查组进行了系统展示。将当地雪亮摄像头工程与天网摄像头工程接入系统中。根据客户需求完成人员智能预警分析，
+        精准将管控人员信息推送到相关人员手机app中。
+      </p>
+      <p class="pastProjectsTitle">警务信息融合平台</p>
+      <p class="pastProjectsText">工作职责：</p>
+      <p class="pastProjectsText" style="textIndent:2em">
+        负责项目整体统筹规划，负责项目组成员工作安排，负责项目技术难点攻克
+      </p>
+      <p class="pastProjectsText">工作业绩：</p>
+      <p class="pastProjectsText" style="textIndent:2em">
+        完成天网，雪亮视频网数据接入，完成警情信息以及警员信息接入
+      </p>
+      <p class="pastProjectsTitle">企业工程管理系统</p>
+      <p class="pastProjectsText">工作职责：</p>
+      <p class="pastProjectsText" style="textIndent:2em">
+        负责项目整体统筹规划，负责项目组成员工作安排，负责服务端的逻辑编写，数据库搭建。
+      </p>
+      <p class="pastProjectsText">工作业绩：</p>
+      <p class="pastProjectsText" style="textIndent:2em">
+        根据甲方需求完成项目功能模块划分，设计数据库model，整理业务流程，编写服务器端接口。
+      </p>
     </div>
     <div class="bottom">
-        <a href="http://www.beian.miit.gov.cn">网站备案号：黔ICP备19005795号-1</a>
+      <a href="http://www.beian.miit.gov.cn">网站备案号：黔ICP备19005795号-1</a>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  components: {},
-  data() {
-    return {
-      headImg: require("@/assets/default.png")
-    };
-  },
-  computed: {},
-  methods: {}
+    components: {},
+    data() {
+        return {
+            headImg: require("@/assets/default.png"),
+        };
+    },
+    computed: {},
+    methods: {},
 };
 </script>
 <style lang='scss' scoped>
@@ -199,19 +234,19 @@ export default {
   align-self: center;
 }
 .pastProjectsTitle {
-  font-size: 16px;
+  font-size: 18px;
   line-height: 30px;
   font-weight: 600;
 }
 .pastProjectsText {
-  font-size: 14px;
+  font-size: 16px;
   line-height: 30px;
 }
 .bottom {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 48px;
-    background-color: #666;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 48px;
+  background-color: #666;
 }
 </style>
